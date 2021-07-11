5bd2ec2073bcc4568db055fdb0173f08cb37d207
Julie <jkent2910@gmail.com>
2021-07-11 16:00:14 -0500

    commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
-  def after_sign_in_path_for(resource)
+  protect_from_forgery with: :exception
 
+  def after_sign_in_path_for(resource)
+    dashboard_path
   end
 end
