31d14c6eab40803a012f792aa6028939a07973e3
Julie <jkent2910@gmail.com>
2021-07-16 22:55:07 -0500

    change time

--- app/controllers/prompt_responses_controller.rb
+++ app/controllers/prompt_responses_controller.rb
@@ -46,7 +46,7 @@ class PromptResponsesController < ApplicationController
 
   def generate_random_time
     date1 = DateTime.now
-    date2 = DateTime.new(2021,10,31)
+    date2 = DateTime.new(2021,9,15)
 
     time = Time.at((date2.to_time.to_f - date1.to_time.to_f)*rand + date1.to_time.to_f)
     return time
